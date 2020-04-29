0fa82a350acccd09e4450849232e7e2aa3e6d80c
Maria Vodolazkaya <vodolazkayamv@gmail.com>
2020-04-29 17:52:16 +0300

    Initial Commit

---
+++ swipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swipe
+//
+//  Created by Masha Vodolazkaya on 29/04/2020.
+//  Copyright © 2020 OpenTek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
